83201fda394ed9aec8107200a76dbdd930d22c98
Mark Liao <mark.liao@live.com>
2018-02-05 13:43:10 -0500

    LinkedIn Button Fix

--- static/js/controller/Home/AboutMeController.js
+++ static/js/controller/Home/AboutMeController.js
@@ -1,4 +1,5 @@
 markusWebAppModule.controller('aboutMeController', function($http, $scope, $rootScope){
-
-	
+	$scope.goToLinkedIn = function(){
+		window.location=href="https://www.linkedin.com/in/liaomark/";
+	};
 });
